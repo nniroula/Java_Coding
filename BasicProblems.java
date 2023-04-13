a3262dcf25d6da9cb8aa890e4d503b461637477f
nabin <nkbhaie2017@gmail.com>
2023-04-13 00:03:25 -0600

    add a function to calculate area of a circle

--- BasicProblems.java
+++ BasicProblems.java
@@ -18,6 +18,22 @@ public class BasicProblems {
         fractionalValue = num1/num2;
         return fractionalValue;
     }
+
+    // 3. find an area of a circle
+    // public static final double APIE = 3.14; // accessible in main method
+    // static final double PIE_VALUE = 3.14;   // accessible in main method
+    private static final double PIE_VAL = 3.14;
+
+    static double getPieValue(){
+        return PIE_VAL;
+    }
+
+    public static double area(double radius){
+        // final double PIE = 3.14;
+        double PIE = getPieValue();
+        double area = PIE * radius*radius;
+        return area;
+    }
     public static void main(String []args){
       
         // 1. Add two numbers
@@ -40,6 +56,7 @@ public class BasicProblems {
         }
         */
 
+        /* 
         // 2. find remainder of two numbers
         System.out.println("Enter your first number: ");
         double firstNum = scanner.nextDouble();
@@ -48,8 +65,20 @@ public class BasicProblems {
         double result = remainder(firstNum, secondNum);
         System.out.printf("The remainder is %f", result);
         System.out.println();
-
-        // 3.
+        */
+        // 3. find an area of a circle
+        // System.out.println(APIE);
+        // System.out.println(PIE_VALUE);
+        // System.out.println(PIE_VAL);
+        try{
+            System.out.print("Enter radius of the circle: ");
+            double radius = scanner.nextDouble();
+            System.out.printf("Area of circle is %.3f", area(radius)); // .3 => 3 digits after decimal point
+            System.out.println();
+        }catch(Exception e){
+            System.out.println("Invalid input data");
+        }
+  
 
         // 4.
     }
